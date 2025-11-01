400b323f265e4e6f3ccf42ceae1fb47592aebfd7
pilxxx <mdpc010407@gmail.com>
2025-11-01 17:32:45 -0300

    Elimino archivo duplicado colectivoTest.cs

--- TarjetaSubeTest/obj/Debug/net5.0/TarjetaSubeTest.AssemblyInfo.cs
+++ TarjetaSubeTest/obj/Debug/net5.0/TarjetaSubeTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TarjetaSubeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685fd7dba7fab87af4e35955fd92cea778ce3229")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee1cad17d322f4663366109ce800f6a7ed125309")]
 [assembly: System.Reflection.AssemblyProductAttribute("TarjetaSubeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TarjetaSubeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
