da2aea07529bb81927f063782d39b622c0364012
pilxxx <mdpc010407@gmail.com>
2025-11-01 17:53:18 -0300

    Agrego tests completos para franquicias

--- TarjetaSube/obj/Debug/net5.0/TarjetaSube.AssemblyInfo.cs
+++ TarjetaSube/obj/Debug/net5.0/TarjetaSube.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TarjetaSube")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685fd7dba7fab87af4e35955fd92cea778ce3229")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+400b323f265e4e6f3ccf42ceae1fb47592aebfd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TarjetaSube")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TarjetaSube")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
